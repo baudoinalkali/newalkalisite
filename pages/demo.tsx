b8487f38814739da6408f13c34c54fb0c6975ce5
Nicholas Baudoin <nick@alkalidesigns.com>
2021-04-26 01:54:31 -0500

    error fix

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -2,14 +2,10 @@ import Modal from '../components/Modal'
 import SiteMap from '../content/siteMap'
 import SiteMapContainer from '../components/SiteMapContainer'
 import Image from 'next/image'
-import TempDesktopNav from '../components/Navigation/TempDesktopNav'
-import CombinedNavigation from '../components/Navigation/CombinedNavigation'
-import TempMobileNav from '../components/Navigation/TempMobileNav'
 
 const Demo = (props) => {
     return (
         <div>
-            <TempMobileNav />
         </div>
     )
 }
