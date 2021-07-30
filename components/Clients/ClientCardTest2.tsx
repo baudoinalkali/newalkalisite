827a02d95926d8b13b3ee746e0a56c2cf07bd4cb
Nicholas Baudoin <nick@alkalidesigns.com>
2021-07-30 00:11:24 -0500

    image component test on client pages

--- components/Clients/ClientCardTest2.tsx
+++ components/Clients/ClientCardTest2.tsx
@@ -11,10 +11,19 @@ const ClientCardTest = props => {
 
     return (
         <a href={`/clients/${props.slug}`}>
-            <div className="group duration-500 rounded shadow-2xl px-4 pb-10 m-auto client-card-bg" style={{ paddingTop: `${clientNameLength()}`, height: "475px", backgroundImage: `url("${props.backgroundImage}")`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundColor: `${props.backgroundColor}` }}>
-                <div className="justify-center p-4 px-6 w-48 m-auto font-medium text-white hover:bg-white hover:bg-opacity-20 rounded-md transition duration-500 ease-in-out border border-white hidden group-hover:flex" style={{ marginTop: `${clientNameLength2()}`}}>View Project</div>
+            <div className="relative">
+                <Image
+                    layout="fill"
+                    className="client-card-image object-center object-cover pointer-events-none"
+                    src={props.backgroundImage}
+                />
+            <div className="client-card-bg" style={{ backgroundColor: `${props.backgroundColor}`,}}>
+            <div className="relative z-1 group duration-500 rounded shadow-2xl px-4 pb-10 m-auto " style={{ paddingTop: `${clientNameLength()}`, height: "475px",}}>
+                <div className="justify-center p-4 px-6 w-48 m-auto font-medium text-white hover:bg-white hover:bg-opacity-20 rounded-md transition duration-500 ease-in-out border border-white hidden group-hover:flex" style={{ marginTop: `${clientNameLength2()}` }}>View Project</div>
                 <div className="text-white group-hover:hidden">{props.industry}</div>
                 <div className="font-bold text-4xl text-white group-hover:hidden">{props.name}</div>
+            </div>
+            </div>
             </div>
         </a>
     )
