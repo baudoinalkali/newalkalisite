9dd5f20ee7b45f0a5cd1d13312aa1782faac3155
Denny Hong <hong961127@gmail.com>
2021-08-08 14:57:45 -0700

    Add provided hs script

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src="//js-na1.hs-scripts.com/14539076.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
