20b0ed40785ca0c6bc0dfd0b4168cab2cc163408
Nicholas Baudoin <nick@alkalidesigns.com>
2021-08-20 22:34:54 -0500

    try hubspot meeting

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 const Demo = (props) => {
     return (
         <div>
+    <div className="meetings-iframe-container" style={{height: "300px"}} data-src="https://meetings.hubspot.com/baudoin?embed=true"></div>
+    <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>
         </div>
     )
 }
