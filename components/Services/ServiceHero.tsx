7c6c0512aa4ca4f343217921844a463a84613f14
Arthur Gloer <arthurgloer@gmail.com>
2021-08-30 11:15:53 -0400

    Not loading particles on ServiceHero component if screen is smaller than 1024px.

--- components/Services/ServiceHero.tsx
+++ components/Services/ServiceHero.tsx
@@ -1,28 +1,43 @@
-import Modal from '../Modal'
-import BGParticles from '../BGParticles'
+import Modal from "../Modal";
+import BGParticles from "../BGParticles";
 
-const ServiceHero = props => {
-    return (
-        <div className="bg-alkali-500">
-            <div className="App px-7" style={{ position: 'relative', overflow: "hidden" }}>
-                <div style={{ position: 'absolute' }}>
-                    <div className="w-screen h-screen invisible lg:visible">
-                        <BGParticles />
-                    </div>
-                </div>
-                <div className="max-w-7xl flex justify-between items-center m-auto py-24 relative">
-                    <div className="w-full lg:w-8/12">
-                        <h1 className="portfolio-title text-white uppercase text-md font-bold">{props.sub}</h1>
-                        <h2 className={`text-4xl md:text-6xl text-white font-bold pt-3 py-10 ${props.titleLength}`}>{props.title}</h2>
-                        <Modal
-                            style="p-3 px-4 font-semibold text-alkali-500 shadow-2xl rounded-md transition duration-500 ease-in-out bg-white hover:bg-gray-100"
-                            text="Schedule a Discovery Call"
-                        />
-                    </div>
-                </div>
-            </div>
+const ServiceHero = (props) => {
+  const isBrowser = () => typeof window !== "undefined";
+  const isSmallScreen = isBrowser() && window.innerWidth < 1024;
+  const particles = isSmallScreen ? (
+    <></>
+  ) : (
+    <div className="w-screen h-screen">
+      <BGParticles />
+    </div>
+  );
+  return (
+    <div className="bg-alkali-500">
+      <div
+        className="App px-7"
+        style={{ position: "relative", overflow: "hidden" }}
+      >
+        <div style={{ position: "absolute" }}>{particles}</div>
+        <div className="max-w-7xl flex justify-between items-center m-auto py-24 relative">
+          <div className="w-full lg:w-8/12">
+            <h1 className="portfolio-title text-white uppercase text-md font-bold">
+              {props.sub}
+            </h1>
+            <h2
+              className={`text-4xl md:text-6xl text-white font-bold pt-3 py-10 ${props.titleLength}`}
+            >
+              {props.title}
+            </h2>
+            <Modal
+              style="p-3 px-4 font-semibold text-alkali-500 shadow-2xl rounded-md transition duration-500 ease-in-out bg-white hover:bg-gray-100"
+              text="Schedule a Discovery Call"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ServiceHero
+export default ServiceHero;
+
