ad1a298848e7c416bb7f54121b3155fcb00f56cd
Nicholas Baudoin <nick@alkalidesigns.com>
2021-09-19 18:32:45 -0500

    form fix

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -2,10 +2,9 @@ export default function About() {
 
     return (
         <div className="">
-            <form>
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" autoComplete="name" required />
-                <button type="submit">Register</button>
+            <form className="footer-form">
+                <input id="email" className="form-input-container form-input form-input-text required " type="Email" name="Email" data-describedby="form-validation-error-box-element-1076" value="" title="Email" data-label-inside="Email" data-at="form-email" placeholder="Email" aria-required="true"/>
+                <button className="email-button btn form-btn item-block" data-at="form-button">Subscribe</button>
             </form>
         </div>
     );
