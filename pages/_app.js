8a3c4cc9e817b95e08d18ca1369a80e2893e60bd
Nicholas Baudoin <nick@alkalidesigns.com>
2021-09-25 02:09:22 -0500

    try gtm npm

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import CookieBanner from "../components/CookieBanner";
 import { Fragment } from "react";
+import { useEffect } from 'react';
 import useFixFouc from "../hooks/useFixFouc";
 import useInitHShChatbot from "../hooks/useInitHShChatbot";
 import dynamic from "next/dynamic";
@@ -9,6 +10,9 @@ const Footer = dynamic(() => import("../components/Footer"));
 function MyApp({ Component, pageProps }) {
   useFixFouc();
   useInitHShChatbot({ delayLoad: 5000 });
+  useEffect(() => {
+    TagManager.initialize({ gtmId: `${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}` });
+}, []);
 
   return (
       <Fragment>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -66,37 +66,6 @@ class MyDocument extends Document {
           />
           {/* End HubSpot Scripts */}
 
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `,
-            }}
-          />
-
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');,
-            });
-          `,
-            }}
-          />
-
 
           {/* Prevent fouc (flash of unstyled component) and transition firing on load for firefox */}
           <style
@@ -115,14 +84,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              <noscript><iframe src=https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          `,
-            }}
-          />
           <Main />
           <NextScript />
 
